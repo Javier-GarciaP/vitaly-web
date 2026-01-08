39dca921198ca0dbb0d38ba737c597cfd565a872
Javier-GarciaP <josejaviergarciap123@gmail.com>
2026-01-08 16:31:04 -0400

    Pruebas

--- src/react-app/components/DashboardLayout.tsx
+++ src/react-app/components/DashboardLayout.tsx
@@ -111,7 +111,7 @@ export default function DashboardLayout() {
                 V
               </div>
               <h1 className="text-lg font-black tracking-tight uppercase italic text-white">
-                Vitaly - Pro
+                Vitaly - Pro 🎅
               </h1>
             </div>
           )}
